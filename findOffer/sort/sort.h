e91eff5fc9bb857bd89351b3d761089740eb2d25
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-08-31 07:52:42 +0000

    add sort dir

---
+++ findOffer/sort/sort.h
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sort.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/29/2014 04:51:05 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  gorden (), 695459065@qq.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef SORT_H
+#define SORT_H
+
+#include <iostream>
+
+void swap(int &data1, int &data2);
+int random_in_range(int start, int end);
+int partition(int data[], int length, int start, int end);
+void quick_sort(int data[], int length, int start, int end);
+
+#endif
