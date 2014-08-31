baba82df5f56308e78ffb55c899d2027d73ff8b2
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-08-31 07:56:54 +0000

    add template dir

---
+++ findOffer/template/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/29/2014 03:03:45 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  gorden (), 695459065@qq.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "template.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << compare(1, 0) << endl;
+    return 0;
+}
