823d14595b255f8de8ff2eb7a3fb369294fa93ee
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-09-01 13:51:56 +0000

    findnum

---
+++ trunk/findOffer/findNum/findnum.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  findNum.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年09月01日 21时35分00秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  gorden5566 (), gorden5566@126.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef FINDNUM_H
+#define FINDNUM_H
+
+#include <stdio.h>
+
+int find_num(int *matrix, int rows, int columns, int number);
+
+#endif
---
+++ trunk/findOffer/findNum/main.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年09月01日 21时33分21秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  gorden5566 (), gorden5566@126.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "findnum.h"
+
+int main()
+{
+    int arr[] = {
+        1, 2, 8, 9,
+        2, 4, 9, 12,
+        4, 7, 10, 13,
+        6, 8, 11, 15
+    };
+
+    int i = 0;
+    for (i = 0; i < 16; ++i) {
+        printf("number:%d, result:%d\n", i, find_num(arr, 4, 4, i));
+    }
+
+    return 0;
+}
