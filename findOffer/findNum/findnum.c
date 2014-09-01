d55eb7594f8cd37d6726ef268fcfacc827aceff7
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-09-01 14:19:39 +0000

    findnum

--- findOffer/findNum/findnum.c
+++ findOffer/findNum/findnum.c
@@ -40,3 +40,43 @@ int find_num(int *matrix, int rows, int columns, int number)
     return found;
 }
 
+int min_in_revolve(int *numbers, int length)
+{
+	if (numbers == NULL || length <= 0) {
+		exit(-1);
+	}
+
+	int index1 = 0;
+	int index2 = length - 1;
+	int indexMid = index1;
+	while (numbers[index1] >= numbers[index2]) {
+		if (index2 - index1 == 1) {
+			indexMid = index2;
+			break;
+		}
+
+		indexMid = (index1 + index2) / 2;
+		if (numbers[index1] == numbers[index2] && numbers[indexMid] == numbers[index1])
+			return min_in_order(numbers, index1, index2);
+		if (numbers[indexMid] >= numbers[index1]) {
+			index1 = indexMid;
+		} else if (numbers[indexMid] <= numbers[index2]) {
+			index2 = indexMid;
+		}
+	}
+
+	return numbers[indexMid];
+}
+
+int min_in_order(int *numbers, int index1, int index2)
+{
+	int result = numbers[index1];
+	int i = index1 + 1;
+	for ( ; i <= index2; ++i) {
+		if (result > numbers[i]) {
+			result = numbers[i];
+		}
+	}
+
+	return result;
+}
--- findOffer/findNum/main.c
+++ findOffer/findNum/main.c
@@ -20,6 +20,7 @@
 
 int main()
 {
+	/*
     int arr[] = {
         1, 2, 8, 9,
         2, 4, 9, 12,
@@ -31,6 +32,11 @@ int main()
     for (i = 0; i < 16; ++i) {
         printf("number:%d, result:%d\n", i, find_num(arr, 4, 4, i));
     }
+	*/
+
+	int revolve_arr[] = {3, 4, 5, 1, 2};
+	int min = min_in_revolve(revolve_arr, 5);
+	printf("%d\n", min);
 
     return 0;
 }
