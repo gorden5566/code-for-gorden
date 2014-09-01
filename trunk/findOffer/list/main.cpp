0b1e1bca2982d4b21ecadcbef033e65585b272fa
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-09-01 06:56:53 +0000

    list dir

--- trunk/findOffer/list/main.cpp
+++ trunk/findOffer/list/main.cpp
@@ -22,10 +22,15 @@ using namespace std;
 int main()
 {
     ListNode *list = NULL;
+    ListNode *list2 = NULL;
 
     //create a list
     for (int i = 0; i < 20; ++i) {
-        add_to_tail(&list, i);
+        if (i & 0x1) {
+            add_to_tail(&list, i);//odd
+        } else {
+            add_to_tail(&list2, i);//even
+        }
     }
 
     //remove a node from list
@@ -33,14 +38,32 @@ int main()
 
     //print list
     cout << "------------------------print list: start-------------------" << endl;
-    print_list(&list);
+    print_list(list);
+    print_list(list2);
     cout << "------------------------print list: end---------------------" << endl;
 
+    //merge two list
+    //ListNode *merged_list = merge_recur(list, list2);
+    ListNode *merged_list = merge_loop(list, list2);
+    print_list(merged_list);
+
+    //ListNode *result = find_kth_to_tail(list, 10);
+    //cout << result->m_nValue << endl;
+    
+    //remove a node from list
+    //remove_node(&list, 10);
+
+    //reverse list
+    //list = reverse_list(list);
+
+
     //print list reversingly
+    /*
     cout << endl << "-----------------print list reversingly: start--------------" << endl;
     print_list_reverse_iter(list);
     print_list_reverse_recur(list);
     cout << endl << "------------------print list reversingly: end---------------" << endl;
+    */
 
     return 0;
 }
