c7560be8de86bb4c7d6700fe521c93f934627d8c
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-09-11 06:50:14 +0000

    string dir

---
+++ trunk/findOffer/string/string.h
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  string.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年09月11日 14时28分03秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  gorden5566 (), gorden5566@126.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef STRING_H
+#define STRING_H
+
+#include <iostream>
+#include <string>
+
+void permutation(char *pstr);
+void permutation(char *pstr, char *pbegin);
+
+#endif
