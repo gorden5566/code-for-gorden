b28970135fd1fc4c193722a6f9a2c9acfd977c95
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-09-12 08:15:51 +0000

    other dir

--- trunk/findOffer/other/main.cpp
+++ trunk/findOffer/other/main.cpp
@@ -19,7 +19,28 @@
 #include "other.h"
 #include <iostream>
 
+void least_numbers();
+void greatest_sum();
+void min_number();
+void ugly_number();
+void first_char();
+
 int main()
+{
+	least_numbers();
+
+	greatest_sum();
+
+	min_number();
+
+	ugly_number();
+
+	first_char();
+
+	return 0;
+}
+
+void least_numbers()
 {
 	int arr[] = {4, 5, 1, 6, 2, 7, 3, 8};
 	int n = sizeof(arr) / sizeof(arr[0]);
@@ -32,6 +53,35 @@ int main()
 		cout << *iter << " ";
 	}
 	cout << endl;
+}
 
-	return 0;
+void greatest_sum()
+{
+	int arr[] = {1, -2, 3, 10, -4, 7, 2, -5};
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	int result = find_greatest_sum_of_sub_array(arr, n);
+	if (result == 0 && g_InvalidInput == true) {
+		cout << "Invalid Input" << endl;
+	} else {
+		cout << result << endl;
+	}
+}
+
+void min_number()
+{
+	int arr[] = {3, 32, 321};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	print_min_number(arr, n);
+}
+
+void ugly_number()
+{
+	cout << get_ugly_number(1500) << endl;
+}
+
+void first_char()
+{
+	char str[] = "abaccdeff";
+	cout << first_not_repeat_char(str) << endl;
 }
