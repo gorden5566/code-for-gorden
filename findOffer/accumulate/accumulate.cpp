8f9e3786a52977185c1a2045709a180039179df5
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-09-13 08:57:43 +0000

    accumulate dir

---
+++ findOffer/accumulate/accumulate.cpp
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  accumulate.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年09月13日 16时44分52秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  gorden5566 (), gorden5566@126.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "accumulate.h"
+
