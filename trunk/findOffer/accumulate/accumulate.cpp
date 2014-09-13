960911f6f75edb1259187c42d495d90c96b17f10
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-09-13 14:50:33 +0000

    accumulate dir

--- trunk/findOffer/accumulate/accumulate.cpp
+++ trunk/findOffer/accumulate/accumulate.cpp
@@ -18,3 +18,41 @@
 
 #include "accumulate.h"
 
+unsigned int Temp::n = 0;
+unsigned int Temp::sum = 0;
+
+A *array[2];
+
+unsigned int solution1(unsigned int n)
+{
+	Temp::reset();
+
+	Temp *a = new Temp[n];
+	delete[] a;
+	a = NULL;
+
+	return Temp::getsum();
+}
+
+unsigned int solution2(unsigned int n)
+{
+	A a;
+	B b;
+	array[0] = &a;
+	array[1] = &b;
+
+	int value = array[1]->sum(n);
+
+	return value;
+}
+
+unsigned int solution3_terminator(unsigned int)
+{
+	return 0;
+}
+
+unsigned int sum_solution3(unsigned int n)
+{
+	static fun f[2] = {solution3_terminator, sum_solution3};
+	return n + f[!!n](n-1);
+}
