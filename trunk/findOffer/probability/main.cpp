8263616bfee58feb8c8158e59f04825c2b55d619
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-09-14 09:16:18 +0000

    probability dir

---
+++ trunk/findOffer/probability/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年09月14日 16时08分31秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  gorden5566 (), gorden5566@126.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "probability.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	print_probability(2);
+
+	return 0;
+}
