45700b43b541b117a1ce820a8ff42448d0cd2267
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-09-19 07:38:45 +0000

    heap 最小堆

---
+++ trunk/findOffer/heap/heap.h
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  heap.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年09月19日 11时21分08秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  gorden5566 (), gorden5566@126.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef HEAP_H
+#define HEAP_H
+
+void min_heap_fixup(int arr[], int i);
+void min_heap_add_number(int arr[], int n, int nNum);
+void min_heap_fixdown(int arr[], int i, int n);
+void make_min_heap(int arr[], int n);
+void min_heap_delete_number(int arr[], int n);
+void min_heap_sort(int arr[], int n);
+
+#endif
