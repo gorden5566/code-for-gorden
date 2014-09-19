2347d4018b361d87d310d039cd56a258a9319348
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-09-19 02:39:13 +0000

    tree 头文件

--- trunk/findOffer/tree/tree.h
+++ trunk/findOffer/tree/tree.h
@@ -25,9 +25,9 @@
 #include <queue>
 
 typedef struct BinaryTreeNode {
-    int                     m_nValue;
-    struct BinaryTreeNode   *m_pLeft;
-    struct BinaryTreeNode   *m_pRight;
+    int              m_nValue;
+    BinaryTreeNode   *m_pLeft;
+    BinaryTreeNode   *m_pRight;
 } BinaryTreeNode;
 
 BinaryTreeNode* construct(int *preorder, int *inorder, int length);
