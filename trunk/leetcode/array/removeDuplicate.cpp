12149f236129febaca0cf6a18a2425250b00334f
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-10-01 03:12:49 +0000

    my leetcode

---
+++ trunk/leetcode/array/removeDuplicate.cpp
@@ -0,0 +1,87 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  removeDuplicate.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年09月29日 21时03分26秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  gorden5566 (), gorden5566@126.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution1
+{
+public:
+	int removeDuplicate(int A[], int n)
+	{
+		if (n == 0)
+			return 0;
+
+		int index = 1;
+		for (int i = 1; i < n; ++i) {
+			if (A[index - 1] != A[i])
+				A[index++] = A[i];
+		}
+
+		return index;
+	}
+};
+
+class Solution2
+{
+public:
+	int removeDuplicate(int A[], int n)
+	{
+		return distance(A, unique(A, A + n));
+	}
+};
+
+class Solution3
+{
+public:
+	int removeDuplicate(int A[], int n)
+	{
+		return removeDuplicate(A, A + n, A) - A;
+	}
+
+	template<typename InIt, typename OutIt>
+	OutIt removeDuplicate(InIt first, InIt last, OutIt output)
+	{
+		while (first != last)
+		{
+			*output++ = *first;
+			first = upper_bound(first, last, *first);
+		}
+
+		return output;
+	}
+};
+
+int main()
+{
+	int a[] = {1, 1, 2};
+	int n = sizeof(a) / sizeof(a[0]);
+
+	Solution1 s1;
+	cout << s1.removeDuplicate(a, n) << endl;;
+
+	Solution2 s2;
+	cout << s2.removeDuplicate(a, n) << endl;;
+
+	Solution3 s3;
+	cout << s3.removeDuplicate(a, n) << endl;;
+
+	return 0;
+}
---
+++ trunk/leetcode/array/rotatedArray.cpp
@@ -0,0 +1,67 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  rotatedArray.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年09月30日 16时00分38秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  gorden5566 (), gorden5566@126.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+	int search(int A[], int n, int target)
+	{
+		int first = 0;
+		int last = n - 1;
+
+		while (first <= last) {
+			int mid = (first + last) / 2;
+
+			if (A[mid] == target)
+				return mid;
+
+			if (A[first] <= A[mid]) { //左边为递增序列
+				if (A[first] <= target && target < A[mid]) { //在左边的区间
+					last = mid - 1;
+				} else { //在右边的区间
+					first = mid + 1;
+				}
+			} else { //右边为递增序列
+				if (A[mid] < target && target <= A[last]) { //在右边
+					first = mid + 1;
+				} else { //在左边
+					last = mid - 1;
+				}
+			}
+		}
+
+		return -1;
+	}
+};
+
+int main()
+{
+	int a[] = {0, 1, 2, 3, 4, 5, 6};
+	//int a[] = {4, 5, 6, 7, 0, 1, 2};
+	int n = sizeof(a) / sizeof(a[0]);
+	
+	Solution s;
+
+	cout << s.search(a, n, 5) << endl;
+	
+	return 0;
+}
