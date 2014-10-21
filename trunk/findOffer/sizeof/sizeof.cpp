209e913079c5746eb1532815a9aefc5ca54704bb
code4gorden@gmail.com <code4gorden@gmail.com@e20e9481-0f46-a5ad-9116-5e720f17f61f>
2014-10-21 14:32:45 +0000

    sizeof 大小

---
+++ trunk/findOffer/sizeof/sizeof.cpp
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  sizeof.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年10月21日 22时20分36秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  gorden5566 (), gorden5566@126.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "sizeof(int): " << sizeof(int) << endl;
+	cout << "sizeof(int*):" << sizeof(int*) << endl;
+	cout << "sizeof(string):" << sizeof(string) << endl;
+
+	return 0;
+}
